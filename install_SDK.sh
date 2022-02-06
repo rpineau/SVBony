1daeee9c39f2d4d07e1b8dc34a839bcaafffb88a
Rodolphe Pineau <pineau@rti-zone.org>
2022-02-06 11:23:51 -0800

    Installed new SDK. Code still complile. Not tested you but that's a good sign

---
+++ install_SDK.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SDK_VERSION=1.6.4
+
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_macOS/lib/x64/libSVBCameraSDK.a static_libs/macOS
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/x64/libSVBCameraSDK.a static_libs/x86_64
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/armv8/libSVBCameraSDK.a static_libs/aarch64/libSVBCameraSDK.a
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/armv7/libSVBCameraSDK.a static_libs/armv7l/libSVBCameraSDK.a
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Windows/lib/x86/SVBCameraSDK.* static_libs/Windows/Win32
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Windows/lib/x64/SVBCameraSDK.* static_libs/Windows/x64
+cp ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_macOS/include/SVBCameraSDK.h .
+
+
