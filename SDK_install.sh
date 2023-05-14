775b992258e0eab87ef481456db81962af4ce638
Rodolphe Pineau <pineau@rti-zone.org>
2023-05-14 12:33:20 -0700

    New SDK 1.11.3 with arm64 support (for when TheSkyX will be also compiled for arm64 on macOS).

--- SDK_install.sh
+++ SDK_install.sh
@@ -2,7 +2,7 @@
 
 SDK_VERSION=$1
 
-cp -fv ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_macOS/lib/x64/libSVBCameraSDK.a static_libs/macOS
+cp -fv ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_macOS/lib/all/libSVBCameraSDK.a static_libs/macOS
 cp -fv ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/x64/libSVBCameraSDK.a static_libs/x86_64
 cp -fv ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/armv8/libSVBCameraSDK.a static_libs/aarch64/libSVBCameraSDK.a
 cp -fv ~/src/Astro/SVBony-SDK-${SDK_VERSION}/SVBCameraSDK_Linux/lib/armv7/libSVBCameraSDK.a static_libs/armv7l/libSVBCameraSDK.a
